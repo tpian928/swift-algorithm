1a65b0a23d5164476514680c2c961b5a9532fae6
tpian928 <tpian928@icloud.com>
2018-10-02 11:05:24 +0800

    init2

---
+++ swift-algorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-algorithm
+//
+//  Created by 王怡飞 on 2018/10/1.
+//  Copyright © 2018年 David Wong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
